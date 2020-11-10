f95a930d6e7487787f9f5e5da4db0ba1e8b357a0
Arinchai <Sasipar.13@mail.kmutt.ac.th>
2020-11-10 17:32:41 +0700

    create class ToyData Method bike

--- src/data/ToyData.java
+++ src/data/ToyData.java
@@ -8,7 +8,9 @@ public class ToyData {
 
     public static Bike[] getBikes() {
         Bike[] bikes = new Bike[3];
-        // your code
+        bikes[0] = new Bike("1818", 3);
+        bikes[1] = new Bike("8888", 2);
+        bikes[2] = new Bike("8181", 1);
 
         return bikes;
     }
